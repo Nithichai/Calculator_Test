80d802c47cfae724946c881bc25cdaae9c553528
NopyJF <Nithichai@users.noreply.github.com>
2017-01-09 23:14:49 +0700

    Update Test_cal.py
    - Add method : postfix infix
    - Delete some method
    - Add comment

--- Test_cal.py
+++ Test_cal.py
@@ -1,67 +1,66 @@
 class Calculator :
     
     def __init__(self, eq):
-        self.eq = eq
-        self.number = []
-        self.operator = []
-        self.x = 0
-        self.y = 0
-        self.op = ""
-        self.ans = 0
-        print(self.eq)
+        self.eq = eq            # Set equation
+        self.number = []        # Number stack
+        self.operator = []      # Operator stack
+        self.ans = 0            # Answer of equation
     
-    def calculate(self):
-        # infix
-        list_eq = self.eq.split(" ")
-        while(len(list_eq) != 0):
-            self.debug()
-            data = list_eq.pop(0)
-            if data == ")" :
-                self.op = self.operator.pop()
-                while(self.op != "("):
-                    self.y = self.number.pop()
-                    self.x = self.number.pop()
-                    self.number.append(str(self.push_data(self.x, self.y, self.op)))
-                    self.op = self.operator.pop()
-            elif (data == "*" or data == "/") :
-                self.y = list_eq.pop(0)
-                if self.y == "+" or self.y == "-" or self.y == "(" :
-                    self.operator.append(data)
-                    self.operator.append(self.y)
-                else :
-                    self.x = self.number.pop()
-                    self.op = data
-                    self.number.append(str(self.push_data(self.x, self.y, self.op)))
-            elif (data == "+" or data == "-" or data == "(") :
-                self.operator.append(data)
-            else :
-                self.number.append(data)
+    def calculate(self):        # Calculate answer
+        self.infix()            # Infix step
+        self.postfix()          # Postfix step
         
-        # postfix
-        while(len(self.operator) != 0):
-            self.op = self.operator.pop()
-            self.y = self.number.pop()
-            self.x = self.number.pop()
-            self.number.append(str(self.push_data(self.x, self.y, self.op)))
-        self.ans = float(self.number.pop())
-        
-    def debug(self):
-        print(self.number, self.operator)
-                
-    def push_data(self, x, y, op):
-        if op == "+":
-            return float(x) + float(y)
-        elif op == "-":
-            return float(x) - float(y)
-        elif op == "*":
-            return float(x) * float(y)
-        elif op == "/":
-            return float(x) / float(y)
+    # Infix step
+    def infix(self):            
+        list_eq = self.eq.split(" ")    # Split string to list
+        while(len(list_eq) != 0):       # No value in list_eq
+            data = list_eq.pop(0)       # Pop data from left
+            if data == ")" :            # Find ")"
+                op = self.operator.pop()    # Pop value from opertor stack
+                while(op != "("):           # op is not "("
+                    y = self.number.pop()   # set x
+                    x = self.number.pop()   # Set y
+                    str_ans = self.get_str_ans(x, y, op)    # Calculate 2 variable and get to string
+                    self.number.append(str_ans)     # Push to number stack
+                    op = self.operator.pop()        # Pop operator stack
+            elif (data == "*" or data == "/") :     # Find "*" or "/"
+                y = list_eq.pop(0)                  # Pop value from left
+                if y == "+" or y == "-" or y == "(" :   # Find "+" or "-" or "("
+                    self.operator.append(data)      # Push data to operator stack
+                    self.operator.append(y)         # Push data to operator stack
+                else :                              # Otherwise
+                    x = self.number.pop()           # Set x
+                    op = data                       # Set op
+                    str_ans = self.get_str_ans(x, y, op)    # Calculate 2 variable and get to string 
+                    self.number.append(str_ans)     # Push answer to number stack
+            elif (data == "+" or data == "-" or data == "(") :  # Find "+" or "-" or "("
+                self.operator.append(data)          # Push data to operator stack
+            else :                                  # Otherwise
+                self.number.append(data)            # Push data to number stack
     
-    def get_ans(self):
-        print(self.ans)
+    # Postfix step
+    def postfix(self):
+        while(len(self.operator) != 0):     # length of operator list is not null
+            op = self.operator.pop()        # Pop operator stack
+            y = self.number.pop()           # Pop number to y
+            x = self.number.pop()           # Pop number to x
+            str_ans = self.get_str_ans(x, y, op)    # Get answer
+            self.number.append(str_ans)     # Push answer to number stack
+        self.ans = float(self.number.pop()) # Set answer
+        print(self.ans)                     # Print answer
+    
+    # Calculate answer            
+    def get_str_ans(self, x, y, op):
+        if op == "+":           # op is plus
+            return str(float(x) + float(y)) # X + Y
+        elif op == "-":         # op is minus
+            return str(float(x) - float(y)) # X - Y
+        elif op == "*":         # op is multiply
+            return str(float(x) * float(y)) # X * Y
+        elif op == "/":         # op is divine
+            return str(float(x) / float(y)) # X / Y
         
-cal = Calculator("2 * ( ( 3 + 8 ) / 4 )")
-cal.calculate()
-cal.get_ans()
+equation = raw_input("Enter equation : ")       # Set equation
+cal = Calculator(equation)                      # Use calculator
+cal.calculate()                                 # Calculate answer
         
